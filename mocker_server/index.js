8062f97db1e63586846199592cdc02c46979831a
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-09 06:51:10 +0800

    vercel config

--- mocker_server/index.js
+++ mocker_server/index.js
@@ -7,5 +7,11 @@ const app = express()
 app.use(cors())
 let PORT = 5001
 
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
+
 apiMocker(app, path.resolve('./mocker/api.js'))
 app.listen(PORT,"localhost", () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
