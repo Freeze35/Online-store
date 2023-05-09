60336d91e53b129206746b90d42bfb34856498a7
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-09 08:41:32 +0800

    vercel config

--- mocker_server/index.js
+++ mocker_server/index.js
@@ -2,16 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const cors = require('cors')
+const apiMocker = require('mocker-api')
 const fs = require("fs");
 const app = express()
 
 app.use(cors())
 let allDevices = JSON.parse(fs.readFileSync(path.join(__dirname, "./mocker/devicesApi.json"), "utf-8"));
-app.get("/api/device", (req, res) => {
+/*app.get("/api/device", (req, res) => {
     return res.json(allDevices);
-});
+});*/
 
-//apiMocker(app, path.resolve('./mocker/api.js'))
+apiMocker(app, path.resolve('./mocker/api.js'))
 app.listen(5001);
 
 module.exports = app;
