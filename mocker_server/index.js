35e8f7a13d688ebc7771c90f175563e724757ac5
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-09 14:02:36 +0800

    vercel mock server

--- mocker_server/index.js
+++ mocker_server/index.js
@@ -10,25 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()) //парсим json формат
 app.use(fileUpload({}))
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://online-store-mocker-server.vercel.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.use("/api", router)
 
 app.get("/:url.webp", (req, res) => {
