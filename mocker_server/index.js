9b1b25e00f6ffd71ef655110bb730b7ddee82f18
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-09 13:18:37 +0800

    vercel mock server

--- mocker_server/index.js
+++ mocker_server/index.js
@@ -5,10 +5,30 @@ const cors = require('cors')
 const app = express()
 const fileUpload = require("express-fileupload")
 
-app.use(cors())
+app.use(cors({origin: '*'}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()) //парсим json формат
 app.use(fileUpload({}))
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use("/api", router)
 
 app.get("/:url.webp", (req, res) => {
