cc7f55dc63625adda230bbfd4e4f044b00c8e282
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-09 13:49:02 +0800

    vercel mock server

--- mocker_server/index.js
+++ mocker_server/index.js
@@ -13,7 +13,7 @@ app.use(fileUpload({}))
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://online-store-mocker-server.vercel.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
