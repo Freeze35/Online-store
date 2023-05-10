d4216b7c0bc5f61b6c50d8be89b9aa2a07a043e8
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-10 13:03:01 +0800

    vercel mock server

--- mocker_server/index.js
+++ mocker_server/index.js
@@ -16,7 +16,25 @@ liveReloadServer.server.once("connection", () => {
 
 app.use(connectLiveReload());
 
-app.use(cors({origin: '*'}))
+app.use(cors({origin: 'https://store-petr-elshin-github-freeze35.netlify.app'}));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://store-petr-elshin-github-freeze35.netlify.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(express.urlencoded({ extended: true }));
 //app.use(express.json()) //парсим json формат
 app.use(fileUpload({}))
