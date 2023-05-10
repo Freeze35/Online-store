2bf13e337a3cb1c1fe75607a6377f1ca504c5046
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-10 16:05:39 +0800

    vercel mock server

--- mocker_server/index.js
+++ mocker_server/index.js
@@ -7,8 +7,9 @@ const fileUpload = require("express-fileupload")
 
 app.use(cors({origin: '*'}))
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()) //парсим json формат
+//app.use(express.json()) //парсим json формат
 app.use(fileUpload({}))
+
 app.use("/api", router)
 
 app.get("/:url.webp", (req, res) => {
@@ -16,6 +17,6 @@ app.get("/:url.webp", (req, res) => {
     res.sendFile(path.resolve(__dirname, `./static/${url}.webp`))
 });
 
-app.listen(5001,"localhost", () => console.log(`Server started on port ${5001}`))
+app.listen(10000,"localhost", () => console.log(`Server started on port ${10000}`))
 
 module.exports = app;
