ae2c8eaf6751a5267531265913f6a762fb0a3059
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-10 15:59:12 +0800

    vercel mock server

--- mocker_server/index.js
+++ mocker_server/index.js
@@ -1,41 +1,14 @@
 const express = require('express');
 const path = require('path');
 const router = require('./routes/index.js')
-
+const cors = require('cors')
 const app = express()
-
-app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = ['http://localhost:3000', 'https://store-petr-elshin-github-freeze35.netlify.app' ];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
-});
-
 const fileUpload = require("express-fileupload")
-const livereload = require("livereload");
-const connectLiveReload = require("connect-livereload");
 
-const liveReloadServer = livereload.createServer();
-liveReloadServer.server.once("connection", () => {
-    setTimeout(() => {
-        liveReloadServer.refresh("/");
-    }, 100);
-});
-
-app.use(connectLiveReload());
-app.use(fileUpload({}))
-//app.use(cors({origin: '*'})) //NOT WORKING IN VERSEL AND RENDER.COM
-
+app.use(cors({origin: '*'}))
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.json()) //парсим json формат
-
-
+app.use(express.json()) //парсим json формат
+app.use(fileUpload({}))
 app.use("/api", router)
 
 app.get("/:url.webp", (req, res) => {
@@ -43,6 +16,6 @@ app.get("/:url.webp", (req, res) => {
     res.sendFile(path.resolve(__dirname, `./static/${url}.webp`))
 });
 
-app.listen(10000,"localhost", () => console.log(`Server started on port ${10000}`))
+app.listen(5001,"localhost", () => console.log(`Server started on port ${5001}`))
 
 module.exports = app;
