4aced0678f90da41e6d29c20240c44fb8553bb6d
Freeze35 <HumanSolidarity@protonmail.com>
2023-05-15 21:37:46 +0800

    Add webpack txt

--- client/src/pages/Shop/Shop.js
+++ client/src/pages/Shop/Shop.js
@@ -28,6 +28,19 @@ const Shop = observer(() => {
         fetchBrands().then(data => device.setBrands(data)) // Загружаем бренды с сервера
         device.setSelectedBrand([])// Чистка выборки Brand при загрузке
         device.setSelectedType([])// Чистка выборки Type при загрузке
+
+
+            setTimeout(fetchDevices(null, null, 1, limitPages).then(data => {
+                device.setDevices(data.rows)
+                device.setTotalCount(data.count)
+                device.setLimit(limitPages)
+                data.rows.map(dev => optionDevice.setTypeBrandListId([...optionDevice.typeBrandListId, {
+                    typeId: dev.typeId,
+                    brandId: dev.brandId
+                }]))
+            }), 500) //timeout for Render.com server
+
+
     }, [])
 
     //Мониторинг изменений брендов или типов(постоянное автоматические запросы)
